7d4b86b747cf98168cf50cb6d4199d3ca67bad6b
mrstelmach <marekstelmach89@gmail.com>
2021-10-07 23:57:42 +0200

    Add longitude and latitude cleaning

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,6 +31,10 @@ def go(args):
     logger.info('Converting last_review to datetime type')
     df['last_review'] = pd.to_datetime(df['last_review'])
     
+    logger.info('Limiting rows with longitude and latitude')
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+    
     logger.info(f'Saving {args.output_artifact} artifact')
     with tempfile.TemporaryDirectory() as tmp_dir:
         file_path = os.path.join(tmp_dir, "clean_sample.csv")
